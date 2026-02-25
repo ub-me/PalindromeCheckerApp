82fffc928271306e4098dc610618ec5ef18ff11d
ub-me <ujjwal.connect.dev@gmail.com>
2026-02-25 23:46:53 +0530

    UC4: Character array palindrome check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -5,18 +5,27 @@ public class PalindromeCheckerApp {
         System.out.println("=================================");
         System.out.println("      PALINDROME CHECKER APP     ");
         System.out.println("=================================");
-        System.out.println("Use Case 3 : Reverse String Method");
+        System.out.println("Use Case 4 : Character Array Method");
 
         String original = "level";
-        String reversed = "";
 
-        // reverse using loop
-        for(int i = original.length() - 1; i >= 0; i--) {
-            reversed = reversed + original.charAt(i);
+        char[] chars = original.toCharArray();
+
+        int start = 0;
+        int end = chars.length - 1;
+
+        boolean isPalindrome = true;
+
+        while(start < end) {
+            if(chars[start] != chars[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
         }
 
-        // compare
-        if(original.equals(reversed)) {
+        if(isPalindrome) {
             System.out.println(original + " is a Palindrome");
         } else {
             System.out.println(original + " is NOT a Palindrome");
