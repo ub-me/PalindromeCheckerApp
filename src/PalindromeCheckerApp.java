f9c8d2fa9f8d7db5762614d0f2b1e636c9217275
ub-me <ujjwal.connect.dev@gmail.com>
2026-02-25 23:28:49 +0530

    UC2: Hardcoded palindrome result

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,12 +2,26 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
+        // UC1 — Welcome Message
         System.out.println("=================================");
         System.out.println("      PALINDROME CHECKER APP     ");
         System.out.println("=================================");
-
         System.out.println("Version : 1.0");
-        System.out.println("Application started successfully!");
 
+        // UC2 — Hardcoded Palindrome Check
+        String word = "madam";   // hardcoded string
+        String reversed = "";
+
+        // Reverse the string
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+
+        // Check palindrome
+        if (word.equals(reversed)) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is NOT a Palindrome");
+        }
     }
 }
