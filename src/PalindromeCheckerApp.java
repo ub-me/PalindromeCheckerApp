9866838e8ed92a4e269c10f1e75e712c0b2bca3b
ub-me <ujjwal.connect.dev@gmail.com>
2026-02-25 23:09:43 +0530

    Base application setup

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,8 @@
+
+    public class PalindromeCheckerApp {
+
+        public static void main(String[] args) {
+            System.out.println("Welcome to Palindrome Checker App");
+        }
+
+    }
